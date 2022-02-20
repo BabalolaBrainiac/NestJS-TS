45b8df79cbaef00531e8da9b2216402b17f84b18
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2022-02-20 20:59:31 +0100

    Updated Auth

--- src/agents/agents.controllers.ts
+++ src/agents/agents.controllers.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { Agent } from './agents.model';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from 'src/auth/auth-local';
+import { JwtAuthGuard } from 'src/auth/jwt-authguard';
 
 @Controller('agent')
 export class AgentController {
@@ -11,5 +13,12 @@ export class AgentController {
   async login(@Request() req) {
     console.log(req.user);
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getAgentProfile(@Request() req) {
+    console.log(req.agent);
+    return req.agent;
   }
 }
